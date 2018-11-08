b83c4d4c8136deca6de7bc5f2f7cd2aeb5ebe682
pengbenran <810648616@qq.com>
2018-11-08 10:45:42 +0800

    guoranmeiwei

---
+++ src/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+// import card from '@/components/card'
+export default {
+  data () {
+    return {
+     
+    }
+  },
+
+  components: {
+    // card
+  },
+
+  methods: {
+   
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+  
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
